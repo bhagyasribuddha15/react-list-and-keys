ae97e1dd5d79cd6dd4040908c3be9e707e631e4d
bhagyasribuddha15 <bhagyasribuddha1633@gmail.com>
2024-01-10 15:18:37 +0000

    react lists and keys

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import UserProfile from './components/UserProfile'
 const userDetailsList = [
   {
     uniqueNo: 1,
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-    name: 'Rahul',
-    role: 'Full Stack Developer',
+    imageUrl:
+      'https://tse2.mm.bing.net/th?id=OIP.RoT4EKppbnPk4k6LAsH1lQHaHa&pid=Api&P=0&h=180',
+    name: 'Surya',
+    role: 'Actor',
   },
   {
     uniqueNo: 2,
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-    name: 'Rahul',
-    role: 'Full Stack Developer',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Raashi',
+    role: 'Fashion Designer',
   },
   {
     uniqueNo: 3,
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-    name: 'Rahul',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Ranjitha',
     role: 'Full Stack Developer',
   },
   {
     uniqueNo: 4,
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-    name: 'Rahul',
-    role: 'Full Stack Developer',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Rehman',
+    role: 'Film Director',
   },
 ]
 const App = () => (
   <div className="list-container">
-    <h1 className="title">Users List</h1>
+    <h1 className="title">Instagram Users List</h1>
     <ul>
       {userDetailsList.map(eachItem => (
         <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
