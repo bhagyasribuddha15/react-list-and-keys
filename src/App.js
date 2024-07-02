e01346a68b5305765cbeaa5a73e13d4114b20633
bhagyasribuddha15 <bhagyasribuddha1633@gmail.com>
2024-07-02 12:37:58 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import UserProfile from './components/UserProfile/index'
 
-const userDetails = {
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Rahul',
-  role: 'Software Developer',
-}
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'John',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Rashi',
+    role: 'Entreprenur',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'williams',
+    role: 'Hair Stylist',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'peter',
+    role: 'Data Scientist',
+  },
+]
 
-const App = () => <UserProfile />
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} />
+      ))}
+    </ul>
+  </div>
+)
 
 export default App
